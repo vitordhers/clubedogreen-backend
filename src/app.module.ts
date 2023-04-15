046101656a70a1080474f48f0857b52006a47430
caicrs <caicrs.contact.com>
2023-04-15 12:31:18 -0300

    Serve-static bug solved

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,16 +9,11 @@ import { AuthModule } from './auth/auth.module';
 import { CaslModule } from './casl/casl.module';
 import { WebhookModule } from './webhook/webhook.module';
 import { ConfigModule } from '@nestjs/config';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true, // no need to import into other modules
-    }),
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'public'),
     }),
     MailModule,
     PrismaModule,
